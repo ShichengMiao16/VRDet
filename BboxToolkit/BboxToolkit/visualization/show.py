b0825e1637f0d37271c2d211e10b7aad312679a5
ShichengMiao16 <hrbeumsc@163.com>
2021-12-31 16:17:09 +0800

    fix a bug

--- BboxToolkit/BboxToolkit/visualization/show.py
+++ BboxToolkit/BboxToolkit/visualization/show.py
@@ -1,6 +1,11 @@
 import cv2
 import time
 import numpy as np
+######################
+# comment the following if you want to show the visualized image
+import matplotlib
+matplotlib.use('Agg')
+######################
 import matplotlib.pyplot as plt
 
 from .colors import colors_val, random_colors
